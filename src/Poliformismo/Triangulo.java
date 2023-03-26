10ac9e5677893b8ca2496b34708308a05f42df89
kevigomez <110569746+kevigomez@users.noreply.github.com>
2023-03-26 18:24:59 -0500

    Update and rename triangulo.java to Triangulo.java

---
+++ src/Poliformismo/Triangulo.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+public  class Triangulo extends Figura {
+    Scanner lectura = new Scanner(System.in);
+
+
+    @Override
+    public double calcularAreaTr() {
+      double base,altura,area;
+
+        System.out.println("digite la base");
+         base= lectura.nextDouble();
+         System.out.println("digite la altura");
+         altura= lectura.nextDouble();
+         area=(base*altura)/2;
+         System.out.println("El area del triangulo es "+area);
+
+         return area;
+
+ 
+
+    }
+
+
+    @Override
+    public double calcularAreaCu() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calcularAreaC'");
+    }
+
+
+    @Override
+    public double calcularAreaCi() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calcularAreaCI'");
+    }
+
+
+
+   
+
+}
