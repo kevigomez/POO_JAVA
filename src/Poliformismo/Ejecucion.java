fe3520ca857ba0f33aa86d62cd351c93774a8e66
kevigomez <110569746+kevigomez@users.noreply.github.com>
2023-03-26 18:48:36 -0500

    Update Ejecucion.java

--- src/Poliformismo/Ejecucion.java
+++ src/Poliformismo/Ejecucion.java
@@ -1,5 +1,12 @@
 package Poliformismo;
 
 public class Ejecucion {
-    
+    public static void main(String[] args) {
+        Reto5 juego = new Reto5();
+        juego.iniciar();
+
+        juego.jugar();
+        juego.finalizar();
+        
+    }
 }
