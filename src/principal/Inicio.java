b4f02d2fef83dcf2cbab6a2e5366d10bc2053141
kevigomez <110569746+kevigomez@users.noreply.github.com>
2023-03-26 19:16:18 -0500

    Update Inicio.java

--- src/principal/Inicio.java
+++ src/principal/Inicio.java
@@ -1,13 +1,22 @@
 package principal;
 
-import salud.Persona;
+import Salud.Persona;
+
 public class Inicio {
+
     public static void main(String[] args) {
-        Persona persona=new Persona();
-        persona.pedirDatos();
-        persona.calcularImc();
-        persona.mostrarPersona();
-        
+        Persona persona1 = new Persona("cc", "20455798", "Santiago", "Mesa", 0, 0, 0, 'm');
+        persona1.pedirDatos();
+        persona1.mostrarPersona();
+        String imcResultado = persona1.calcularImc();
+        if (imcResultado.equals("PESOBAJO")) {
+            System.out.println("El peso está por debajo de lo ideal.");
+        } else if (imcResultado.equals("PESOIDEAL")) {
+            System.out.println("El peso es ideal.");
+        } else {
+            System.out.println("Tiene sobrepeso.");
+        }
+persona1.mayorEdad();
     }
     
 }
